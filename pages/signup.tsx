4cd22163146236afd045249126b9eae5c089cfe8
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-08-22 22:30:31 -0300

    updating signup page with changes

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -4,21 +4,17 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../public/images/betalogo.svg';
-import add_image_icon from '../public/add_image_icon.svg';
-import React, { InputHTMLAttributes, useState } from 'react';
+import { useState } from 'react';
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-    avatarprops: File;
-}
 
-const SignUp = ({ avatarprops }: Props) => {
+const SignUp = () => {
 
-    const [avatar, setAvatar] = useState('')
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm_Password, setConfirm_Password] = useState('');
 
-    /*   const handleAvatar = (e: React.InputHTMLAttributes<HTMLInputElement>) => {
-          setAvatar()
-      }
-   */
     return (
         <>
             <header className={styles.container}>
@@ -45,20 +41,41 @@ const SignUp = ({ avatarprops }: Props) => {
                     <h1 className={styles.tittle}>Perfil Verdadeiro</h1>
                     <p className={styles.sub}>*Informações pessoais sensíveis não serão públicas dentro da plataforma.</p>
                     <form>
-                        <label htmlFor='avatar' className={styles.label_container}>
-                            <input type='file' id='avatar' name='avatar' className={styles.input_avatar} />
+                        <ul className={styles.ulForm}>
+                            <li className={`${styles.liItens} ${styles.liNameItem}`}>
+                                <label htmlFor="name" className={`${styles.allLabels} ${styles.NameLabel}`}>Nome</label>
+                                <input className={`${styles.inputName} ${styles.inputAll}`} id="name" type="text" value={name} name='name' onChange={e => { setName(e.target.value) }} placeholder="Digite o seu nome" />
+                            </li>
+                            <li className={`${styles.liItens} ${styles.liSurnameItem} `}>
+                                <label htmlFor="surname" className={`${styles.allLabels} ${styles.surnameLabel} ${styles.inputSurname}`}>Sobrenome</label>
+                                <input className={`${styles.inputSurname} ${styles.inputAll}`} id="surname" type="text" value={surname} name='surname' onChange={e => { setSurname(e.target.value) }} placeholder="Digite o seu sobrenome" />
+                            </li>
+                            <li className={`${styles.liItens} ${styles.liEmailItem} `}>
+                                <label htmlFor="email" className={`${styles.allLabels} ${styles.EmailLabel} ${styles.inputEmail}`}>Email</label>
+                                <input className={`${styles.inputEmail} ${styles.inputAll}`} id="email" type="email" value={email} name='email' onChange={e => { setEmail(e.target.value) }} placeholder="Digite o seu email" />
+                            </li>
+                            <li className={`${styles.liItens} ${styles.liSenhaItem} `}>
+                                <label htmlFor="senha" className={`${styles.allLabels} ${styles.senhaLabel} ${styles.inputSenha}`}>Senha</label>
+                                <input className={`${styles.inputSenha} ${styles.inputAll}`} id="senha" type="password" value={password} name='senha' onChange={e => { setPassword(e.target.value) }} placeholder="Digite uma senha" />
+                            </li>
+                            <li className={`${styles.liItens} ${styles.liConfirmaSenhaItem} `}>
+                                <label htmlFor="confirmasenha" className={`${styles.allLabels} ${styles.confirmaSenhaLabel} ${styles.inputConfirmaSenha}`}>Confirmar Senha</label>
+                                <input className={`${styles.inputConfirmaSenha} ${styles.inputAll}`} id="confirmasenha" type="password" value={confirm_Password} name='confirmasenha' onChange={e => { setConfirm_Password(e.target.value) }} placeholder="confirme a senha" />
+                            </li>
 
-
-                            <div className={styles.img_icon}>
-                                <Image
-                                    src={add_image_icon}
-                                    width={55}
-                                    height={55}
-                                />
-                            </div>
-                            <p className={styles.input_text}>Clique para adicionar uma imagem</p>
-
-                        </label>
+                            {/*           <li class="twoColumnPart">
+                                <label for="email">Email</label>
+                                <input id="email" type="text" value="" name="email">
+                            </li>
+                            <li>
+                                <label for="subject">Subject</label>
+                                <input id="subject" type="text" value="" name="subject">
+                            </li>
+                            <li>
+                                <label for="message">Message</label>
+                                <textarea id="message" type="text" name="message"></textarea>
+                            </li> */}
+                        </ul>
                     </form>
                 </div>
             </main>
