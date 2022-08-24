7e682b27c4f7dcbcf8813558dc7a5c9e3c3b706d
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-08-24 18:35:28 -0300

    new

---
+++ components/Schaduale/Schaduale.tsx
@@ -0,0 +1,11 @@
+import styles from '../Schaduale/Schaduale.module.css';
+
+const Schaduale = () => {
+    return (
+        <div>
+            ...
+        </div>
+    )
+}
+
+export { Schaduale };
